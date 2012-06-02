12d58ea04029b6b72c199ba22222da9db09be436
aerie.pinkpanther@hotmail.fr <aerie.pinkpanther@hotmail.fr@d73a0aad-e4b6-85c9-31ff-9adfde00bd66>
2012-06-02 21:18:30 +0000

    TXT to SQL DB done

--- Schallmar/Server/db/fr/item_db2.sql
+++ Schallmar/Server/db/fr/item_db2.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
---
--- Serveur: 127.0.0.1
--- Généré le : Sam 02 Juin 2012 à 15:33
--- Version du serveur: 5.1.54
--- Version de PHP: 5.3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données: `db`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `item_db2`
---
 
 CREATE TABLE IF NOT EXISTS `item_db2` (
   `id` smallint(5) unsigned NOT NULL DEFAULT '0',
